5d15cc9529415c535992844569e9e6e838e714d0
Hetkumar Prajapati <hetgoraj@gmail.com>
2024-05-03 21:11:11 +0530

    simple dynamic URLs

--- firstproject/urls.py
+++ firstproject/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('about-us/',views.aboutUs),
     path('course/',views.courses),
+    path('courseStr/<str:courseid>',views.courseDetails1),
+    path('courseInt/<int:courseid>',views.courseDetails2),
+    path('courseSlug/<slug:courseid>',views.courseDetails3),
+    path('courseAny/<courseid>',views.courseDetails4),
 ]
--- firstproject/views.py
+++ firstproject/views.py
@@ -4,3 +4,12 @@ def aboutUs(req):
     return HttpResponse("<h1><b>Welcome to MyPage</b></h1>")
 def courses(req):
     return HttpResponse("<h1><b>Welcome to CoursePage</b></h1>")
+
+def courseDetails1(req,courseid):
+    return HttpResponse(courseid)
+def courseDetails2(req,courseid):
+    return HttpResponse(courseid)
+def courseDetails3(req,courseid):
+    return HttpResponse(courseid)
+def courseDetails4(req,courseid):
+    return HttpResponse(courseid)
